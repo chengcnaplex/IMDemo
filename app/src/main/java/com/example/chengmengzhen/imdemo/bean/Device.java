fb442be0d290780ece0a14f2e16b520675421817
chengcnaplex <chengcn_aplex@aliyun.com>
2016-07-21 18:44:40 +0800

    add retrofit

---
+++ app/src/main/java/com/example/chengmengzhen/imdemo/bean/Device.java
@@ -0,0 +1,8 @@
+package com.example.chengmengzhen.imdemo.bean;
+
+/**
+ * Created by APLEX on 2016/7/21.
+ */
+public class Device {
+    
+}
---
+++ app/src/main/java/com/example/chengmengzhen/imdemo/url/DeviceUrl.java
@@ -0,0 +1,54 @@
+package com.example.chengmengzhen.imdemo.url;
+
+import com.example.chengmengzhen.imdemo.bean.Device;
+
+import java.util.List;
+
+import retrofit.Call;
+import retrofit.http.GET;
+
+/**
+ * Created by APLEX on 2016/7/21.
+ */
+public interface DeviceUrl {
+    /*****************************************************************
+     * Retrofit 2.0 只能定义这样的模式(如下)
+     * <p/>
+     * // Synchronous Call in Retrofit 2.0
+     * Call<Contributor> call = GitHubUrl.contributors();
+     * Contributor contributor = call.execute();
+     * <p/>
+     * // Synchronous Call in Retrofit 2.0
+     * Call<Contributor> call = GitHubUrl.contributors();
+     * call.enqueue(new Callback<Contributor>() {
+     *
+     * @Override public void onResponse(Response<Contributor> response) {
+     * Get result Repo from response.body()
+     * }
+     * @Override public void onFailure(Throwable t) {
+     * <p/>
+     * }
+     * });
+     ********************************************************************/
+    @GET("Restful/rest/finduser}")
+    Call<List<Device>> users();
+
+
+    /*****************************************************************
+     *
+     *         Retrofit 1.9 同步请求
+     *       Synchronous in Retrofit 1.9
+     *  @GET("repos/{owner}/{repo}/contributors")
+     *  List<Contributor> contributors(@Path("owner") String owner,@Path("repo") String repo);
+     *
+     * *************************************************************/
+
+    /*****************************************************************
+     *
+     *         Retrofit 1.9 异步请求
+     *      Asynchronous in Retrofit 1.9
+     *  @GET("repos/{owner}/{repo}/contributors")
+     *  void contributors( @Path("owner") String owner, @Path("repo") String repo,Callback<Contributor> cb);
+     *
+     * *************************************************************/
+}
